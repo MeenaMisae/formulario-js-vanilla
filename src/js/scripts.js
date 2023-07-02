0e958ff0cceab2a0857c09dd8a9a34002bb456d3
Meena Misae <meenahiwatashi@gmail.com>
2023-07-01 22:55:05 -0300

    parte da lógica js feita

---
+++ src/js/scripts.js
@@ -0,0 +1,36 @@
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+const getLetterLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+  const symbols = "(){}[]=<>/,..!@#$%&*+-";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+const generatePassword = (
+  getLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) => {
+  let password = "";
+
+  const passwordLength = 10;
+
+  const generators = [];
+};
+
+generatePasswordButton.addEventListener("click", () => {
+  console.log("click");
+});
